6549ca4cf8a0d1f52ce6dc741f8dee04819876c9
NickNash <nicholas.nash+123@gmail.com>
2012-09-05 09:12:44 +0100

    Unify forward & backward ray-marching

--- src/quat_julia/naive_raymarch.clj
+++ src/quat_julia/naive_raymarch.clj
@@ -2,9 +2,6 @@
   "Very naive ray-marching for the quaternion julia set"
   (:use quat-julia.vec-utils))
 
-
-; Julia set
-
 (defn outside-julia? [q c max-iterations] 
   (loop [iter 0 z q]
     (if (> (vec-length z) 4) 
@@ -13,27 +10,24 @@
         (recur (inc iter) (vec-add (quat-mul z z) c)) 
         false))))
 
-; Naive Ray marching
-
-(defn ray-march-forward [outside? initial-z max-z num-steps]
-  (let [dz (float (/ (- max-z initial-z) num-steps))]
-    (loop [z initial-z]  
-      (if (and (outside? z) (< z max-z)) 
-        (recur (+ z dz)) z)))) 
-
-(defn ray-march-backward [inside? initial-z min-z max-z num-steps]
+(defn ray-march-z [keep-marching? get-next-z initial-z min-z max-z num-steps]
   (let [dz (float (/ (- max-z min-z) num-steps))]
-    (loop [z initial-z]
-      (if (and (inside? z) (> z min-z)) 
-        (recur (- z dz)) z))))
+    (loop [z initial-z]  
+      (if (keep-marching? z) 
+        (recur (get-next-z z dz)) z)))) 
 
-(defn ray-march [outside? min-z max-z
+(defn ray-march-forward-then-backward [outside? min-z max-z
                  num-coarse-steps num-fine-steps] 
-  (let [z (ray-march-forward outside? min-z max-z num-coarse-steps)]
-    (if (< z max-z) (ray-march-backward #(not (outside? %)) z min-z max-z num-fine-steps) max-z)))
+  (let [z (ray-march-z #(and (outside? %) (< % max-z)) 
+                       #(+ %1 %2) 
+                       min-z min-z max-z num-coarse-steps)]
+    (if (< z max-z) (ray-march-z #(and (not (outside? %)) (> % min-z))
+                                 #(- %1 %2)
+                                 z min-z max-z num-fine-steps) max-z)))
 
 (defn ray-march-julia [c max-iterations param 
                        num-coarse-steps num-fine-steps 
                        min-z max-z x y]
   (letfn [(outside? [z] (outside-julia? [x y z param] c max-iterations))]
-    (ray-march outside? min-z max-z num-coarse-steps num-fine-steps)))
+    (ray-march-forward-then-backward 
+      outside? min-z max-z num-coarse-steps num-fine-steps)))
